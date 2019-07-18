7a5dcc0acf30ab78a61712b890d285ae6868ee8b
beans_pc <ujt2h8@gmail.com>
2019-07-19 07:30:30 +0800

    feat : should_return_parkingLot_when_post_to_API

--- src/test/java/com/thoughtworks/parking_lot/parkingLotsControllerTest.java
+++ src/test/java/com/thoughtworks/parking_lot/parkingLotsControllerTest.java
@@ -6,9 +6,11 @@ import com.thoughtworks.parking_lot.model.parkingLot;
 import com.thoughtworks.parking_lot.repository.parkingLotRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -16,7 +18,14 @@ import org.springframework.test.web.servlet.ResultActions;
 
 import javax.transaction.Transactional;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -29,7 +38,7 @@ public class parkingLotsControllerTest {
     public void contextLoads() {
     }
 
-
+    @MockBean
     private com.thoughtworks.parking_lot.repository.parkingLotRepository parkingLotRepository;
 
     @Transactional
@@ -49,5 +58,35 @@ public class parkingLotsControllerTest {
                         "\t\"capacity\":4,\n" +
                         "\t\"location\":\"zhongshan\"\n" +
                         "}"));
+    }
+
+
+    @Test
+    public void should_return_parkingLot_when_get_to_API() throws Exception {
+        parkingLot parkingLot = new parkingLot();
+
+        List<parkingLot> parkingLotList = new ArrayList<>();
+
+
+        parkingLot.setName("myHome");
+        parkingLot.setCapacity(4);
+        parkingLot.setLocation("zhongshan");
+
+        parkingLotList.add(parkingLot);
+        Mockito.when(parkingLotRepository.findAll()).thenReturn(parkingLotList);
+
+
+        ObjectMapper objectMapper = new ObjectMapper();  // object转string
+
+        mockMvc.perform(get("/parking-lots"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().json(("[\n{\n" +
+                        "\t\"name\":\"myHome\",\n" +
+                        "\t\"capacity\":4,\n" +
+                        "\t\"location\":\"zhongshan\"\n" +
+                        "}\n]")));
+
+
     }
 }
